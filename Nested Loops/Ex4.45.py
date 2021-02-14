bcb02c4a092b554cfdb473f4d5372573659ef8fc
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-14 20:49:44 +0530

    commit

--- Nested Loops/Ex4.45.py
+++ Nested Loops/Ex4.45.py
@@ -22,3 +22,15 @@ for i in range(5):
     print()
     for j in range(5): 
         print("*",end='')
+
+# Write a program to print the following pattern
+# *
+# **
+# ***
+# ****
+# *****
+
+for i in range(1,6):
+    print()
+    for j in range(i):
+        print("*", end=' ')
