445fd14355f66c9eef31c7f8d40f9324103d016a
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-14 22:03:25 +0530

    commit

--- Nested Loops/Ex4.45.py
+++ Nested Loops/Ex4.45.py
@@ -79,9 +79,10 @@ for i in range(1,5):
 #  1234
 # 12345  
 
-
-
-
-
-
-
+N = 5
+for i in range(1,N+1):
+    for k in range(N,i,-1):
+        print(" ", end=' ')
+    for j in range(1,i+1):
+        print(j, end=' ')
+    print()        
