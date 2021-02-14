9d0403561621474af1ed9425a79fa2fd231f0802
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-14 20:41:23 +0530

    commit

---
+++ Decision Control Statement/Nested Loops/Ex4.45.py
@@ -0,0 +1,24 @@
+# Write a program to print the following pattern.
+# Pass 1- 1 2 3 4
+# Pass 2- 1 2 3 4
+# Pass 3- 1 2 3 4
+# Pass 4- 1 2 3 4
+# Pass 5- 1 2 3 4
+
+for i in range(1,6):
+    print("PASS",i,"-  ",end=' ')
+    for j in range(1,6):
+        print(j, end=' ')
+    print()    
+
+# Write a program to print the following pattern
+# **********
+# **********
+# **********
+# **********
+# **********
+
+for i in range(5):
+    print()
+    for j in range(5): 
+        print("*",end='')
