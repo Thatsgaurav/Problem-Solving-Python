b182cf721c29fb117787a4502b7472d36d34a64a
Parampreet Singh <76559816+Param302@users.noreply.github.com>
2021-03-02 16:44:18 +0530

    9th question company salary bonus....
    The question says that if the employee is male then he will get 5% bonus on his salary. If the employee's salary is less than 10000 then the employee will get 2% additional bonus on his salary. 
    It means that if the employee is male then he will get 5% bonus on his salary and if the salary is less than 10000 then he will get 2% additional bonus on his salary.
    If the employee is not male then she will get only 2% bonus on his salary.
    But According to your program if the employee is male then he will get 5% bonus on his salary.
    else she will get 10% bonus on his salary.
    So, that's the mistake in your program hope you understand the logic. 👍 :)

--- Decision Control Statement/Ex4.1.py
+++ Decision Control Statement/Ex4.1.py
@@ -70,15 +70,18 @@ else:
 
 ch = input("Enter the sex of the employee (m or f) : ")
 sal = int(input("Enter the salary of the employee : "))
+bonus = 0
 if (ch=='m'):
-    bonus = 0.05*sal
+    bonus += ((5/100)*sal)
+    if (sal<10000):
+        bonus += ((2/100)*sal)
 else:
-    bonus = 0.10*sal
-amt_to_be_paid = sal+bonus
-print(" Salary = ",sal)
-print(" Bonus = ",bonus)
-print(" ************************ ")
-print("Amount to be paid : ",amt_to_be_paid)        
+    if (sal<10000):
+        bonus += ((2/100)*sal)
+print('\n','*'*20)
+print('Bonus: ',bonus)
+print('Total Amount is',sal+bonus)
+print('*'*20)
 
 # Write a program to find whether a given year is a leap year or not.
 
