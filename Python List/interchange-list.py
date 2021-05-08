c27febea1a3dd75bfc0d0f634e84c4bc7f8b9018
Gaurav Singh <0201csai117@niet.co.in>
2021-05-09 04:13:25 +0530

    commit

--- Python List/interchange-list.py
+++ Python List/interchange-list.py
@@ -1,17 +1,5 @@
 # Python program to interchange first and last elements in a list
 
-# Given a list, write a Python program to swap first and last element of the list.
-
-# Examples:
-
-# Input: [12, 35, 9, 56, 24]
-# Output: [24, 35, 9, 56, 12]
-
-# Input: [1, 2, 3]
-# Output: [3, 2, 1]
-# 1: Find the length of the list and simply swap the first element with (n-1)th element. Approach
-
-
 def swapList(newList):
     newList[0], newList[-1] = newList[-1], newList[0]
     return newList
